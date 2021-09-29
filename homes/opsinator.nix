8378066091216b1ecf0eadb711bf82a8fc6c1e81
Maarten van den Berg <maarten@channable.com>
2021-09-29 18:58:27 +0200

    opsinator: Enable i3

--- homes/opsinator.nix
+++ homes/opsinator.nix
@@ -6,7 +6,7 @@
     ~/scratch/zeteldeuk/modules/zsh.nix
     ~/scratch/zeteldeuk/modules/tmux.nix
     ~/scratch/zeteldeuk/modules/alacritty.nix
-    # ~/code/zeteldeuk/modules/wm.nix
+    ~/scratch/zeteldeuk/modules/wm.nix
     ~/scratch/zeteldeuk/modules/nvim.nix
     ~/.config/nixpkgs/home-untracked.nix
   ];
@@ -27,13 +27,38 @@
       rofi
     ];
 
-    programs.zsh.sessionVariables.NIX_PATH = "$HOME/.nix-defexpr/channels\${NIX_PATH:+:}$NIX_PATH";
+    targets.genericLinux.enable = true;
+    programs.zsh.sessionVariables.NIX_PATH =
+      "$HOME/.nix-defexpr/channels\${NIX_PATH:+:}$NIX_PATH";
 
-    programs.zsh.shellAliases = {
-      "gssh" = "gcloud compute ssh";
-    };
+    programs.zsh.shellAliases = { "gssh" = "gcloud compute ssh"; };
 
     # programs.git.userEmail is untracked.
+
+    xsession.windowManager.i3.config.terminal =
+      pkgs.lib.mkForce "~/.nix-profile/bin/nixGL alacritty";
+
+    programs.autorandr.enable = true;
+    programs.autorandr.hooks.preswitch = {
+      "rename" = ''
+        set -x
+        env | grep AUTORANDR
+        IFS=':' read -a outputs <<< "$AUTORANDR_MONITORS"
+
+        for index in "''${!outputs[@]}"; do
+          output_name="''${outputs[index]}"
+          echo "making monitor $output_name index $index"
+          # xrandr --setmonitor "$index" auto "$output_name"
+        done
+
+        # i3-msg reload
+        set +x
+      '';
+    };
+
+    systemd.user.systemctlPath = "/bin/systemctl";
+
+    services.screen-locker.i3lockPath = "/usr/bin/i3lock";
 
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
