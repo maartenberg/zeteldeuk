ca435a61466785e2a1e681217c3571fe5c2e8e67
Maarten van den Berg <maartenberg1+git@gmail.com>
2022-09-25 13:45:14 +0200

    Add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Home Manager config";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations.opstop = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./homes/opstop.nix
+        ];
+      };
+    };
+}
