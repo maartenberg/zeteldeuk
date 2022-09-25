8ad9c594869c18bf3f86baa52c76d4e2c44d1e45
Maarten van den Berg <maartenberg1+git@gmail.com>
2022-09-25 16:27:13 +0200

    opstop: Add untracked files properly

--- flake.nix
+++ flake.nix
@@ -13,19 +13,28 @@
       url = "file:///home/maarten/.config/nixpkgs/home-untracked.nix";
       flake = false;
     };
+    consul-tunnels = {
+      type = "file";
+      url = "file:///home/maarten/.config/nixpkgs/consul-tunnels.nix";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, home-manager, untracked, ... }:
+  outputs = { nixpkgs, home-manager, untracked, consul-tunnels, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
       homeConfigurations.opstop = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
         modules = [
           ./homes/opstop.nix
-          untracked
+          (import untracked)
+          (import consul-tunnels)
         ];
       };
     };
